34ad3820b0136c51416ee6bddbfd6081f0e23a37
Mateusz Kulinowski <mateusz.kulinowski@euvic.pl>
2021-04-16 18:30:02 +0200

    Add project files.

---
+++ Workshop/Contracts/IArticleManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Workshop.Entities;
+
+namespace Workshop.Contracts
+{
+    public interface IArticleManager
+    {
+        Task<int> Create(Article article);
+        Task<int> Delete(int Id);
+        Task<int> Count(string search);
+        Task<int> Update(Article article);
+        Task<Article> GetById(int Id);
+        Task<List<Article>> ListAll(int skip, int take, string orderBy, string direction, string search);
+    }
+}
