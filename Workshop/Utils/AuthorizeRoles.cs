364a482af3cc5ab7058e39672f7b1c19a5031873
Jakub Cieślik <jakucie936@student.polsl.pl>
2021-04-30 16:14:59 +0200

    code cleaning

---
+++ Workshop/Utils/AuthorizeRoles.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Workshop.Enums;
+
+namespace Workshop.Utils
+{
+    public class AuthorizeRoles : AuthorizeAttribute
+    {
+        public AuthorizeRoles(params Role[] roles)
+        {
+            Roles = string.Join(",", Enum.GetNames(typeof(Role)));
+        }
+    }
+}
