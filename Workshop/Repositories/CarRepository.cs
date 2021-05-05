959c04863ff7ddc29b931147d43441343b4648f3
mateada966 <mateuszadamczyk@poczta.fm>
2021-05-05 13:02:48 +0200

    Cars Page v1 (Only Page)

---
+++ Workshop/Repositories/CarRepository.cs
@@ -0,0 +1,12 @@
+﻿using Workshop.DataAccess;
+using Workshop.Entities;
+using Workshop.Interfaces.Repositories;
+using Workshop.Repositories.Base;
+
+namespace Workshop.Repositories
+{
+    public class CarRepository : RepositoryBase<Car>, ICarRepository
+    {
+        public CarRepository(CarContext context) : base(context) { }
+    }
+}
