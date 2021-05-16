ab256511af66c982c462b9f7ee779ab7faa3f7c7
Jakub Cieślik <jakucie936@student.polsl.pl>
2021-05-16 10:32:36 +0200

    fixed connection error when trying to access endpoints too fast, added badges utils

---
+++ Workshop/Utils/Badges.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Workshop.Enums;
+
+namespace Workshop.Utils
+{
+    public static class Badges
+    {
+        public static string GetBadge(Status status) => status switch
+        {
+            Status.Pending => "secondary",
+            Status.InProgress => "warning",
+            Status.Finished => "success",
+            Status.Canceled => "danger",
+            _ => "dark"
+        };
+    }
+}
